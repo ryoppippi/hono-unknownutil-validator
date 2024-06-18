d0b4aa9269710b55d346dadebef59cfa9168ff89
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-06-18 17:41:17 +0100

    feat: Implement uValidator and update dependencies
    This commit introduces a new utility function 'uValidator' which is used
    for validation in the Hono framework. It also updates the project
    dependencies, adding new ones such as '@core/unknownutil' and
    '@hono/hono', and updates the test cases to reflect these changes.

--- mod.ts
+++ mod.ts
@@ -1,3 +1,86 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
+import type {
+  Context,
+  Env,
+  MiddlewareHandler,
+  TypedResponse,
+  ValidationTargets,
+} from "@hono/hono";
+import { validator } from "@hono/hono/validator";
+
+import * as U from "@core/unknownutil";
+
+// deno-lint-ignore ban-types
+export type Hook<T, E extends Env, P extends string, O = {}> = (
+  result: Result<T>,
+  c: Context<E, P>,
+) =>
+  | Response
+  | Promise<Response>
+  | void
+  | Promise<Response | void>
+  | TypedResponse<O>;
+
+type Result<T = unknown> =
+  | { data: T; success: true }
+  | { data: T; success: false; error: Omit<U.AssertError, "cause"> };
+
+// deno-lint-ignore no-explicit-any
+type Schema<T = any> = U.Predicate<T>;
+type OutputType<T> = T extends U.Predicate<infer O>
+  ? undefined extends O ? never : O
+  : never;
+
+export const uValidator = <
+  S extends Schema,
+  O extends OutputType<S>,
+  Target extends keyof ValidationTargets,
+  E extends Env,
+  P extends string,
+  V extends {
+    in: { [K in Target]: O };
+    out: { [K in Target]: O };
+  } = {
+    in: { [K in Target]: O };
+    out: { [K in Target]: O };
+  },
+>(
+  target: Target,
+  schema: S,
+  hook?: Hook<O, E, P>,
+): MiddlewareHandler<E, P, V> =>
+  validator(target, (value, c) => {
+    let result: Result<U.PredicateType<S>>;
+
+    try {
+      result = { data: U.ensure(value, schema), success: true };
+    } catch (_error: unknown) {
+      const error = _error as unknown as U.AssertError;
+      result = {
+        data: value,
+        success: false,
+        error: {
+          message: error.message,
+          name: error.name,
+          stack: error.stack,
+        },
+      };
+    }
+
+    if (hook) {
+      const hookResult = hook(result, c);
+      if (hookResult) {
+        if (hookResult instanceof Response || hookResult instanceof Promise) {
+          return hookResult;
+        }
+        if ("response" in hookResult) {
+          return hookResult.response;
+        }
+      }
+    }
+
+    if (!result.success) {
+      return c.json(result, 400);
+    }
+
+    return result.data;
+  });
--- mod_test.ts
+++ mod_test.ts
@@ -1,8 +1,157 @@
 import { test } from "@cross/test";
-import { assertEquals } from "@std/assert";
+import { assertEquals, assertFalse, assertNotEquals } from "@std/assert";
+import type { IsExact } from "type-testing";
 
-import { add } from "./mod.ts";
+import { Hono } from "@hono/hono";
+import type { StatusCode } from "@hono/hono/utils/http-status";
 
-test("dummy", () => {
-  assertEquals(add(1, 2), 3);
+import * as U from "@core/unknownutil";
+import { uValidator } from "./mod.ts";
+
+type ExtractSchema<T> = T extends Hono<infer _, infer S> ? S : never;
+
+const isAuthor = U.isObjectOf({
+  name: U.isString,
+  age: U.isNumber,
+});
+
+type Author = U.PredicateType<typeof isAuthor>;
+
+const isItem = U.isObjectOf({
+  id: U.isNumber,
+  title: U.isString,
+});
+
+type Item = U.PredicateType<typeof isItem>;
+function generateApp() {
+  const app = new Hono();
+
+  const route = app.post("/author", uValidator("json", isAuthor), (c) => {
+    const data = c.req.valid("json");
+    return c.json({
+      success: true,
+      message: `${data.name} is ${data.age}`,
+    });
+  });
+
+  type Actual = ExtractSchema<typeof route>;
+  type Expected = {
+    "/author": {
+      $post: {
+        input: {
+          json: Author;
+        };
+        output: {
+          success: boolean;
+          message: string;
+        };
+        outputFormat: "json";
+        status: StatusCode;
+      };
+    };
+  };
+
+  type verify = IsExact<Expected, Actual> extends true ? true : never;
+
+  return app;
+}
+
+test("Should return 200 response", async () => {
+  const app = generateApp();
+  const req = new Request("http://localhost/author", {
+    body: JSON.stringify(
+      {
+        name: "Superman",
+        age: 20,
+      } as const satisfies Author,
+    ),
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+  });
+  const res = await app.request(req);
+  assertNotEquals(res, null);
+  assertEquals(res.status, 200);
+  assertEquals(await res.json(), {
+    success: true,
+    message: "Superman is 20",
+  });
+});
+
+test("Should return 400 response", async () => {
+  const app = generateApp();
+  const req = new Request("http://localhost/author", {
+    body: JSON.stringify({
+      name: "Superman",
+      age: "20",
+    }),
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+  });
+  const res = await app.request(req);
+
+  assertNotEquals(res, null);
+  assertEquals(res.status, 400);
+
+  const data = (await res.json()) as { success: boolean };
+  assertFalse(data["success"]);
+});
+
+function generateAppWithHook() {
+  const app = new Hono();
+
+  app.post(
+    "/post",
+    uValidator("json", isItem, (result, c) => {
+      if (!result.success) {
+        return c.text(`${result.data.id} is invalid!`, 400);
+      }
+      const data = result.data;
+      return c.text(`${data.id} is valid!`);
+    }),
+    (c) => {
+      const data = c.req.valid("json");
+      return c.json({
+        success: true,
+        message: `${data.id} is ${data.title}`,
+      });
+    },
+  );
+
+  return app;
+}
+
+test("[With Hook]: Should return 200 response", async () => {
+  const app = generateAppWithHook();
+  const req = new Request("http://localhost/post", {
+    body: JSON.stringify(
+      {
+        id: 123,
+        title: "Hello",
+      } as const satisfies Item,
+    ),
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+  });
+
+  const res = await app.request(req);
+  assertNotEquals(res, null);
+  assertEquals(res.status, 200);
+  assertEquals(await res.text(), "123 is valid!");
+});
+
+test("[With Hook]: Should return 400 response", async () => {
+  const app = generateAppWithHook();
+  const req = new Request("http://localhost/post", {
+    body: JSON.stringify({
+      id: "123",
+      title: "Hello",
+    }),
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+  });
+
+  const res = await app.request(req);
+  assertNotEquals(res, null);
+  assertEquals(res.status, 400);
+  assertEquals(await res.text(), "123 is invalid!");
 });
